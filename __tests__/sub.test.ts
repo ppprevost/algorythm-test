877939992de3b41cc82ae19541c290d4cc997eca
Pierre-Philippe Prevost <ppprevost@aol.fr>
2020-01-22 16:27:50 +0100

    test: array number closest to zero test

--- __tests__/sub.test.ts
+++ __tests__/sub.test.ts
@@ -1,9 +1,18 @@
 import sub from '../src/sub'
 
 describe('sub', () => {
-  describe("exaggerate", () => {
-    test("add '!' to the end the designated number times", () => {
-      expect(sub.exaggerate('Yippee', 8)).toBe('Yippee!!!!!!!!')
+  describe('closestToZero', () => {
+    test('it must return 0', () => {
+      expect(sub.closestToZero([])).toEqual(0)
+    })
+    test('it must return 5', () => {
+      expect(sub.closestToZero([8, 5, 10])).toEqual(5)
+    })
+    test('it must return -1', () => {
+      expect(sub.closestToZero([5, 4, -9, 6, -10, -1, 8])).toEqual(-1)
+    })
+    test('it must return 2', () => {
+      expect(sub.closestToZero([8, 2, 3, -2])).toEqual(2)
     })
   })
 })
--- src/sub.ts
+++ src/sub.ts
@@ -1,5 +1,19 @@
-const exaggerate = (str: string, level: number): string => str + '!'.repeat(level)
+const closestToZero = (n: number[]) => {
+  let actual = n[0]
+  if (!n || !n.length) {
+    return 0
+  }
+  for (let i = 0; i < n.length; i++) {
+    actual =
+      Math.abs(n[i]) <= Math.abs(actual)
+        ? Math.abs(n[i]) === actual
+          ? actual
+          : n[i]
+        : actual
+  }
+  return actual
+}
 
 export default {
-  exaggerate,
+  closestToZero,
 }
